b19638d06b7d1a390dde000f3b9d4cc282d366b3
0amfibinator0 <00amfibiantor00@mail.ru>
2022-11-01 17:56:39 +0300

    урок6

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -12,12 +12,22 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
       <div className="pizza-block__selector">
         <ul>
           {types.map((type) => (
-            <li onClick={() => setActiveType(type)} className={activeType === type ? 'active' : ''}>{typeNames[type]}</li>
+            <li
+              key={type}
+              onClick={() => setActiveType(type)}
+              className={activeType === type ? 'active' : ''}>
+              {typeNames[type]}
+            </li>
           ))}
         </ul>
         <ul>
           {sizes.map((size, i) => (
-            <li onClick={() => setActiveSize(i)} className={activeSize === i ? 'active' : ''}>{size} см.</li>
+            <li
+              key={size}
+              onClick={() => setActiveSize(i)}
+              className={activeSize === i ? 'active' : ''}>
+              {size} см.
+            </li>
           ))}
         </ul>
       </div>
